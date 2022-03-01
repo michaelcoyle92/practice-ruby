e087d25e6c67028acb4d9ed312e14135ddc5e6a6
maccoyle@gmail.com <michaelcoyle@michaels-mbp.myfiosgateway.com>
2022-03-01 14:36:12 -0500

    added initial hashes

---
+++ store_item.rb
@@ -0,0 +1,8 @@
+item1 = {:color => "red", :price => 7, :condition => "used" }
+item2 = {color: "blue", price: 9, condition: "new" }
+item3 = {color: "majenta", price: 42, condition: "new"}
+
+
+p item1
+p item2
+p item3
